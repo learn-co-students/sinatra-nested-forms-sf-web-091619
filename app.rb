61a84897c2f3aac22e62c1db6414179d19862440
Max Zakzook <mzakzook@me.com>
2019-10-08 22:56:04 -0700

    Done.

--- app.rb
+++ app.rb
@@ -5,5 +5,31 @@ module FormsLab
 
     # code other routes/actions here
 
+    
+    get '/' do
+      erb :'/pirates/index'
+    end
+
+    get '/new' do
+      erb :'/pirates/new'
+    end
+
+    # get '/pirates/show' do
+    #   erb :'/pirates/show'
+    # end
+
+    # get '/pirates/:name' do
+    #   binding.pry
+    #   @pirate = Pirate.all.find {|pirate| pirate.name == params[:name]}
+    #   erb :'/pirates/show'
+    # end
+
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+      @ship1 = Ship.new(params[:pirate][:ships][0])
+      @ship2 = Ship.new(params[:pirate][:ships][1])
+      erb :'/pirates/show'
+    end
+
   end
 end
