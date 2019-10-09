6f2771a53ac45b72ca92acdf1c85b4b9370f848d
Mustafa Muse <umpp101@icloud.com>
2019-10-09 10:55:30 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,9 +1,32 @@
 require './environment'
 
+
 module FormsLab
   class App < Sinatra::Base
+    # set :views, "views"
 
-    # code other routes/actions here
+    get '/' do
+      # @pirate = Pirate.all
+      erb :'/pirates/index'
+    end
+
+    get '/new' do
+      erb :'/pirates/new'
+    end
+
+    get '/show' do
+    #  @pirate = Pirate.find(params[:id])
+      erb :'/pirates/show'
+    end
+
+    post '/pirates' do
+      # binding.pry
+      @pirate= Pirate.new(params[:pirate])
+      @ship1= Ship.new(params[:pirate][:ships][0])
+      @ship2= Ship.new(params[:pirate][:ships][1])
+      erb :"/pirates/show"
+    end
 
   end
+
 end
